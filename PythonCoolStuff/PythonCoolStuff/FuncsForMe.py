bd71c0c6382295ad118a8b121b870c970a94a91b
AdForcer <vdkuziakin@gmail.com>
2024-11-27 16:09:10 +0300

    Added some stuff
    Basic sheeesh as writing files

--- PythonCoolStuff/PythonCoolStuff/FuncsForMe.py
+++ PythonCoolStuff/PythonCoolStuff/FuncsForMe.py
@@ -16,6 +16,19 @@ def LoadFiles(Path, FilesAmount):
         Files.append(File.readlines())
         File.close()
     return Files
+def WriteAFile(Path, File_ID, Information):
+    File = open(f'{Path}\{File_ID}.penis','w')
+    File.write(Information)
+    File.close()
+def CopyFile(Path, File_ID, New_File_ID):
+    Original_File = open(f'{Path}\{File_ID}.penis', 'r')
+    Original_Information = Original_File.readlines()
+    Original_File.close()
+    tmp = ''
+    for i in range(len(Original_Information)):
+        tmp+=Original_Information[i]
+    WriteAFile(Path,New_File_ID,tmp)
+
 
 
     
--- PythonCoolStuff/PythonCoolStuff/PythonCoolStuff.py
+++ PythonCoolStuff/PythonCoolStuff/PythonCoolStuff.py
@@ -1,6 +1,7 @@
 from FuncsForMe import *
 FilesAmount, Path = startUp()
 #FilesAmount and path are loaded from conf file
+CopyFile(Path,0,1)
 while True:
     print('Menu of my engine:')
     print('0. Exit')
